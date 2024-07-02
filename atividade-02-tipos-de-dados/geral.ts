6e3082d790812bea3bf37dfdd36bc7ff25aedfca
Willian Gabriel <willianlopes20@hotmail.com>
2024-07-01 21:54:17 -0300

    concluido atividade 2

---
+++ atividade-02-tipos-de-dados/geral.ts
@@ -0,0 +1,11 @@
+// Questão 1
+
+console.log("Uma variável na programação é um espaço na memória do computador que armazena valores que podem ser alterados durante a execução do programa.")
+
+// Questão 2
+
+console.log("CONST - variáveis que não mudarão o seu valor, deve começar sendo declarado o seu valor// LET - variáveis que podem ter seu valor atualizado durante o programa// VAR - variáveis com escopo de função ou global e permite reatribuição, mas pode causar problemas devido ao seu comportamento de escopo.")
+
+// Questão 3
+
+console.log("OBJETO É uma propriedade, onde cada propriedade é composta por uma chave (string ou símbolo) e um valor (de qualquer tipo a sua escolha)// LISTA É uma coleção ordenada de valores, onde cada valor é acessado por um índice numérico, usado para armazenar listas de dados em uma sequência ordenada. ")
