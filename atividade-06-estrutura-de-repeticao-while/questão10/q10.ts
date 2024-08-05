52c4cc9f447f56fc04d76acaa88b4b1d7610b649
Willian Gabriel <102621293+williangabriell@users.noreply.github.com>
2024-08-05 15:49:38 -0300

    Delete atividade-06-estrutura-de-repeticao-while directory

--- atividade-06-estrutura-de-repeticao-while/questão10/q10.ts
+++
@@ -1,9 +0,0 @@
-let tabuada = 9
-let contadorQ10 = 0
-
-while (contadorQ10 <= 9) {
-    let valorFinal = tabuada * contadorQ10
-    contadorQ10++
-    console.log(`9 x ${contadorQ10} = ${valorFinal} `)
-}
-
