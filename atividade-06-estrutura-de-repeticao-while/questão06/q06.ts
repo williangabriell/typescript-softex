a1f81211abdb099b5028cf5404075ed0a286041e
AlanGarci4 <alangarciasantos@outlook.com>
2024-08-05 15:29:20 -0300

    atividade 6 iniciada

---
+++ atividade-06-estrutura-de-repeticao-while/questão06/q06.ts
@@ -0,0 +1,7 @@
+let i = 0
+
+while (i < 10) {
+    i++ 
+    console.log(i)     
+}
+
---
+++ atividade-06-estrutura-de-repeticao-while/questão10/q10.ts
@@ -0,0 +1,9 @@
+let tabuada = 9
+let contadorQ10 = 0
+
+while (contadorQ10 <= 9) {
+    let valorFinal = tabuada * contadorQ10
+    contadorQ10++
+    console.log(`9 x ${contadorQ10} = ${valorFinal} `)
+}
+
